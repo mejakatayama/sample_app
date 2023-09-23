5fdbb3133b574018b89d4a62d6505fa34be8b368
ketchupeden <ketchupeden@gmail.com>
2023-09-23 12:15:45 +0000

    modified mail addresses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample-app.onrender.com'
+  host = 'sample-app-mznv.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
